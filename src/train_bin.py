35cb2d05c41dc0d0d90479e3addbdae651238b79
saattrupdan <saattrupdan@gmail.com>
2022-05-18 11:37:03 +0100

    feat: Loop over models

--- src/train_bin.py
+++ src/train_bin.py
@@ -13,23 +13,31 @@ def main():
     test = load_dataset(dataset_id, split='test')
 
     # Get the trainer
-    trainer, test = get_bin_trainer(
-        train=train,
-        val=val,
-        test=test,
-        pretrained_model_id='Maltehb/aelaectra-danish-electra-small-cased',
-        new_model_id='saattrupdan/grammar-checker-da'
-    )
+    models = [
+        'KBLab/bert-base-swedish-cased-new',
+        'NbAiLab/nb-bert-base',
+        'Maltehb/danish-bert-botxo',
+        'vesteinn/IceBERT',
+        'TurkuNLP/bert-base-finnish-cased-v1',
+        'vesteinn/ScandiBERT'
+    ]
+    for model in models:
+        trainer, test = get_bin_trainer(
+            train=train,
+            val=val,
+            test=test,
+            pretrained_model_id=model,
+            new_model_id='saattrupdan/grammar-checker-da'
+        )
 
-    # Train the model
-    trainer.train()
+        # Train the model
+        trainer.train()
 
-    # Evaluate the model on the test set
-    metrics = trainer.evaluate(test)
+        # Evaluate the model on the test set
+        metrics = trainer.evaluate(test)
 
-    print(metrics)
-
-    breakpoint()
+        print(model)
+        print(metrics)
 
 
 if __name__ == '__main__':
