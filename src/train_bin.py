b1fcf83918ba18b096331f69bc46cc5aa7a279f7
saattrupdan <saattrupdan@gmail.com>
2022-05-18 14:24:47 +0100

    feat: Allow multiple arguments

--- src/train_bin.py
+++ src/train_bin.py
@@ -13,65 +13,67 @@ from .utils import NeverLeaveProgressCallback
 def main():
 
     # Get arguments
-    language = sys.argv[1]
-    print(f'Benchmarking {language} dataset')
+    languages = sys.argv[1:]
 
-    # Disable logging
-    disable_progress_bar()
-    tf_logging.set_verbosity_error()
-    ds_logging.set_verbosity_error()
-    logging.getLogger('filelock').setLevel(logging.ERROR)
-    logging.getLogger('absl').setLevel(logging.ERROR)
-    logging.getLogger('transformers.trainer').setLevel(logging.ERROR)
+    for language in languages:
+        print(f'Benchmarking {language} dataset')
 
-    # Load the datasets
-    dataset_id = f'saattrupdan/grammar-correction-{language}'
-    train = load_dataset(dataset_id, split='small_train')
-    val = load_dataset(dataset_id, split='val')
-    test = load_dataset(dataset_id, split='test')
-
-    # Get the trainer
-    models = [
-        'KBLab/bert-base-swedish-cased-new',  # da=0%, nb=0%, nn=%, sv=68%, is=0%, fo=0%
-        'NbAiLab/nb-bert-base',  # da=46%, nb=66%, nn=%, sv=61%, is=4%, fo=0%
-        'Maltehb/danish-bert-botxo',  # da=24%, nb=5%, nn=%, sv=0%, is=0%, fo=0%
-        'vesteinn/IceBERT',  # da=0%, nb=0%, nn=%, sv=0%, is=0%, fo=42%
-        'TurkuNLP/bert-base-finnish-cased-v1',  # da=0%, nb=0%, nn=%, sv=0%, is=0%, fo=%
-        'vesteinn/ScandiBERT'  # da=56%, nb=62%, nn=%, sv=62%, is=61%, fo=%
-    ]
-    for model in models:
-        trainer, new_test = get_bin_trainer(
-            train=train,
-            val=val,
-            test=test,
-            pretrained_model_id=model,
-            new_model_id='saattrupdan/grammar-checker-da'
-        )
-
-        # Set transformers logging back to error
+        # Disable logging
+        disable_progress_bar()
         tf_logging.set_verbosity_error()
+        ds_logging.set_verbosity_error()
+        logging.getLogger('filelock').setLevel(logging.ERROR)
+        logging.getLogger('absl').setLevel(logging.ERROR)
+        logging.getLogger('transformers.trainer').setLevel(logging.ERROR)
 
-        # Remove trainer logging
-        trainer.log = lambda _: None
+        # Load the datasets
+        dataset_id = f'saattrupdan/grammar-correction-{language}'
+        train = load_dataset(dataset_id, split='small_train')
+        val = load_dataset(dataset_id, split='val')
+        test = load_dataset(dataset_id, split='test')
 
-        # Remove the callback which prints the metrics after each evaluation
-        trainer.remove_callback(PrinterCallback)
+        # Get the trainer
+        models = [
+            'KBLab/bert-base-swedish-cased-new',  # da=0%, nb=0%, nn=%, sv=68%, is=0%, fo=0%
+            'NbAiLab/nb-bert-base',  # da=46%, nb=66%, nn=%, sv=61%, is=4%, fo=0%
+            'Maltehb/danish-bert-botxo',  # da=24%, nb=5%, nn=%, sv=0%, is=0%, fo=0%
+            'vesteinn/IceBERT',  # da=0%, nb=0%, nn=%, sv=0%, is=0%, fo=42%
+            'TurkuNLP/bert-base-finnish-cased-v1',  # da=0%, nb=0%, nn=%, sv=0%, is=0%, fo=%
+            'vesteinn/ScandiBERT'  # da=56%, nb=62%, nn=%, sv=62%, is=61%, fo=%
+        ]
+        for model in models:
+            trainer, new_test = get_bin_trainer(
+                train=train,
+                val=val,
+                test=test,
+                pretrained_model_id=model,
+                new_model_id='saattrupdan/grammar-checker-da'
+            )
 
-        # Remove the progress bar callback
-        trainer.remove_callback(ProgressCallback)
+            # Set transformers logging back to error
+            tf_logging.set_verbosity_error()
 
-        trainer.add_callback(NeverLeaveProgressCallback)
+            # Remove trainer logging
+            trainer.log = lambda _: None
 
-        # Train the model
-        trainer.train()
+            # Remove the callback which prints the metrics after each evaluation
+            trainer.remove_callback(PrinterCallback)
 
-        # Evaluate the model on the test set
-        metrics = trainer.evaluate(new_test)
+            # Remove the progress bar callback
+            trainer.remove_callback(ProgressCallback)
 
-        print(model)
-        print(metrics)
+            trainer.add_callback(NeverLeaveProgressCallback)
 
-    print()
+            # Train the model
+            trainer.train()
+
+            # Evaluate the model on the test set
+            metrics = trainer.evaluate(new_test)
+
+            print(model)
+            print(metrics)
+
+        print()
 
 
 if __name__ == '__main__':
