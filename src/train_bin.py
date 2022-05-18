d521fab16ea5ad08cb4fec5efcdc5383b588d803
saattrupdan <saattrupdan@gmail.com>
2022-05-18 12:54:12 +0100

    feat: Change to sv

--- src/train_bin.py
+++ src/train_bin.py
@@ -12,19 +12,19 @@ def main():
     tf_logging.set_verbosity_error()
 
     # Load the datasets
-    dataset_id = 'saattrupdan/grammar-correction-nb'
+    dataset_id = 'saattrupdan/grammar-correction-sv'
     train = load_dataset(dataset_id, split='small_train')
     val = load_dataset(dataset_id, split='val')
     test = load_dataset(dataset_id, split='test')
 
     # Get the trainer
     models = [
-        'KBLab/bert-base-swedish-cased-new',  # da=0%, nb=%, sv=%, is=%
-        'NbAiLab/nb-bert-base',  # da=46%, nb=%, sv=%, is=%
-        'Maltehb/danish-bert-botxo',  # da=24%, nb=%, sv=%, is=%
-        'vesteinn/IceBERT',  # da=0%, nb=%, sv=%, is=%
-        'TurkuNLP/bert-base-finnish-cased-v1',  # da=%, nb=%, sv=%, is=%
-        'vesteinn/ScandiBERT'  # da=%, nb=%, sv=%, is=%
+        'KBLab/bert-base-swedish-cased-new',  # da=0%, nb=0%, sv=%, is=%
+        'NbAiLab/nb-bert-base',  # da=46%, nb=66%, sv=%, is=%
+        'Maltehb/danish-bert-botxo',  # da=24%, nb=5%, sv=%, is=%
+        'vesteinn/IceBERT',  # da=0%, nb=0%, sv=%, is=%
+        'TurkuNLP/bert-base-finnish-cased-v1',  # da=0%, nb=0%, sv=%, is=%
+        'vesteinn/ScandiBERT'  # da=56%, nb=62%, sv=%, is=%
     ]
     for model in models:
         trainer, new_test = get_bin_trainer(
