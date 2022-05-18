d4675d4bf1ff74c5de01d4c11b1381a2f51252f0
saattrupdan <saattrupdan@gmail.com>
2022-05-18 11:32:54 +0100

    feat: Up batch size, set val_loss as metric

--- src/trainer.py
+++ src/trainer.py
@@ -61,13 +61,13 @@ def get_bin_trainer(train: Dataset,
         save_strategy='epoch',
         report_to='none',
         save_total_limit=1,
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=8,
+        per_device_train_batch_size=32,
+        per_device_eval_batch_size=32,
         learning_rate=2e-5,
         num_train_epochs=100,
         warmup_ratio=0.1,
-        gradient_accumulation_steps=4,
-        metric_for_best_model='mcc',
+        gradient_accumulation_steps=1,
+        metric_for_best_model='val_loss',
         load_best_model_at_end=True
     )
 
